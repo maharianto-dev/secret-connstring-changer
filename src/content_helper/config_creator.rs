6223b676bde0c59013ec60d19f39aa4bbddf5a6d
Finsa Anugrah Pratama <finsa.pratama@qualysoft-coe.com>
2024-11-18 10:45:53 +0700

    fixing config_creator

--- src/content_helper/config_creator.rs
+++ src/content_helper/config_creator.rs
@@ -1,61 +1,43 @@
 use std::{
     env, 
     error::Error,
-    fs::{self, File}, path::{Path, PathBuf},
+    fs::File, 
+    path::{Path, PathBuf},
 };
 
 pub struct ConfigCreator {
     file_name: String,
     file_extension: String,
-    dir: PathBuf,
-    is_success: bool,
     message: String,
 }
 
 impl ConfigCreator {
     pub fn new(_file_name: String, _file_ext: String) -> Result<Self, Box<dyn Error>> {
-        let current_exe_path = env::current_exe()?;
-        let curr_dir = current_exe_path.parent().unwrap();
-
-        let mut is_success: bool = false;
-        let mut message: String = "File not found!".to_owned();
-        let mut directory = curr_dir.to_path_buf();
-        let contents = fs::read_dir(curr_dir)?;
-        for content in contents {
-            let dir_content = &content?;
-            if *dir_content.file_name() == *format!("{}.{}",_file_name,_file_ext) {
-                is_success = true;
-                message = "File exist!".to_owned();
+        let curr_dir = env::current_dir()?;
+        let full_filename = format!("{}.{}",&_file_name,&_file_ext);
+        let config_file = PathBuf::from(curr_dir.join(Path::new(&full_filename)));
+        match config_file.exists() && config_file.is_file() {
+            true => Ok(Self {
+                file_name: _file_name,
+                file_extension: _file_ext,
+                message: "File exist!".to_string(),
+            }),
+            false => {
+                ConfigCreator::generate_new_file(&config_file)?;
+                return Ok(Self {
+                    file_name: _file_name,
+                    file_extension: _file_ext,
+                    message: format!("{} success to generate!", &full_filename),
+                });
             }
+        
         }
-        Ok(Self {
-            file_name: _file_name,
-            file_extension: _file_ext,
-            dir: directory,
-            is_success: is_success,
-            message: message,
-        })
+        
     }
-    pub fn is_success(&self) -> bool {
-        return self.is_success;
-    }
-    pub fn get_message(&self) -> String {
-        return self.message.clone();
-    }
-    pub fn generate_new_file(&mut self) -> Result<(), Box<dyn Error>> {
-        let file_name = format!("{}.{}",self.file_name, self.file_extension);
-        println!("Generating File {}...", file_name);
-        let file_path = self.dir.join(file_name);
-        match File::create(file_path) {
-            Ok(_) => {
-                self.is_success = true;
-                self.message = format!("{}.{} success to create!", self.file_name, self.file_extension);
-                
-                Ok(())
-            }
-            Err(error) => {
-                return Err(Box::new(error));
-            }
-        }
+    
+    fn generate_new_file(path: &PathBuf) -> Result<(), Box<dyn Error>> {
+        println!("Generating File {:?}...", path.file_name().expect("File not found!"));
+        File::create(path)?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,40 +11,34 @@ fn main() {
     let args = ProgramArgs::new();
     let config_creator = ConfigCreator::new("Config".to_string(),"json".to_string());
     match config_creator {
-        Ok(mut con_creator) => {
-            if con_creator.is_success() {
-                println!("{}",con_creator.get_message());
-            } else {
-                con_creator.generate_new_file().unwrap();
-                println!("{}",con_creator.get_message());
-            };
+        Ok(_) => {
+            match JsonConfig::new(args.connstring_key().to_string()) {
+                Ok(json) => match json.get_config_connection_string() {
+                    Ok(conn_string) => {
+                        let dir_crawler = DirCrawler::new(&String::from(args.secret_dir()));
+                        match dir_crawler.validate().is_ok() {
+                            true => {
+                                let _ = dir_crawler.run_crawler(&conn_string);
+                                println!("Done changing connection string");
+                                println!("Graceful shutdown");
+                            }
+                            false => {
+                                eprintln!("{}", dir_crawler.validate().message());
+                                eprintln!("panic!");
+                            }
+                        }
+                    }
+                    Err(error) => {
+                        eprintln!("Error reading config file!");
+                        eprintln!("panic!");
+                    }
+                },
+                Err(error) => eprintln!("JSON Config is not valid!"),
+            }
         },
         Err(error) => {
             eprintln!("{}", error);
             eprintln!("panic!");
         }
-    }
-    match JsonConfig::new(args.connstring_key().to_string()) {
-        Ok(json) => match json.get_config_connection_string() {
-            Ok(conn_string) => {
-                let dir_crawler = DirCrawler::new(&String::from(args.secret_dir()));
-                match dir_crawler.validate().is_ok() {
-                    true => {
-                        let _ = dir_crawler.run_crawler(&conn_string);
-                        println!("Done changing connection string");
-                        println!("Graceful shutdown");
-                    }
-                    false => {
-                        eprintln!("{}", dir_crawler.validate().message());
-                        eprintln!("panic!");
-                    }
-                }
-            }
-            Err(error) => {
-                eprintln!("Error reading config file!");
-                eprintln!("panic!");
-            }
-        },
-        Err(error) => eprintln!("JSON Config is not valid!"),
     }
 }
